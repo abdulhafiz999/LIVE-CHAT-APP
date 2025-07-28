926147982121a6ad35584e94efe67a4f01516677
Hafiz tech <abdulhafiz99888@gmail.com>
2025-07-28 01:26:45 +0100

    Done with the ChatContainer

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,8 @@
 
 
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router()
 
@@ -9,8 +10,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
-// router.get("/check", protectRoute, checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router
