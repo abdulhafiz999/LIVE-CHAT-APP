2fc0520008c5a6d1a00f98e6525c04a14ffa75ca
Hafiz tech <abdulhafiz99888@gmail.com>
2025-07-28 16:06:51 +0100

    Stil on the socket io

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
-}
+} 
 
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMessages, getUsersForSidebar } from "../controllers/messaage.controller.js"
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/messaage.controller.js"
 
 const router = express.Router()
 
 router.get("/users", protectRoute, getUsersForSidebar)
 router.get("/:id",protectRoute, getMessages )
 
+router.post("/send/:id", protectRoute,sendMessage);
 
 export default router;
