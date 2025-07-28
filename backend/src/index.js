af8eeb614420f8a8c63f45a50e80d3b9709d665e
Hafiz tech <abdulhafiz99888@gmail.com>
2025-07-28 21:16:19 +0100

    removed some comments

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,3 @@
-// import express from "express";
-// import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
-// import cors from "cors";
-
-// import dotenv from "dotenv";
-// import { connect } from "mongoose";
-// import { connectDB } from "./lib/db.js";
-// import cookieParser from "cookie-parser";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(express.json({ limit: "10mb" }));
-// app.use(cookieParser())
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:5175",
-//     credentials: true,
-//   })
-// );
-
-// const PORT = process.env.PORT || 5002;
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT} ✅✅🎉`);
-//   connectDB();
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
