725cf00cbc90299bacdf403934f4d849e4ded580
Hafiz tech <abdulhafiz99888@gmail.com>
2025-07-30 02:14:49 +0100

    Resolved some errors in the packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
