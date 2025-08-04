bd2db6dc177a5bed4b059328d9d51db25a33f3fa
Hafiz tech <abdulhafiz99888@gmail.com>
2025-08-04 18:20:30 +0100

    minor changes, deploying on render

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,9 +17,9 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to Hack-Chat!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a conversation from the sidebar to start chatting 😊
         </p>
       </div>
     </div>
